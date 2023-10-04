d249a982c3ed290bf7ad4ce7e391a75bfaacfd3c
Mehdi Key <formk@kermed.fr>
2023-10-04 16:36:22 +0200

    Opereration sur les nombres

--- index.php
+++ index.php
@@ -1,7 +1,53 @@
 <?php
-
+// phpinfo();
 require './fonctions.php';
 
 $titre = "Operateurs php";
+
+/*
+operateurs
++
+-
+*
+/
+**
+%
+*/
+
+$x = 4;
+$y = 2;
+$z = 5;
+
+// $calcul = $x % $y;
+// $calcul = ($x + $z) * $y;
+
+// $x += 5; // $x = $x + 5
+// $x -= 5; // $x = $x - 5
+// $x *= 5; // $x = $x * 5
+// $x /= 5; // $x = $x / 5
+// $x **= 5; // $x = $x ** 5
+// $x %= 5; // $x = $x % 5
+
+// dd($x);
+
+// $i = 0;
+// $i++; // $i = $i + 1; $i += 1;
+
+// $i--; // $i = $i - 1; $i -= 1;
+
+// dd($i);
+
+// $anneeDeNaissance = 1990;
+// $age = date("Y") - $anneeDeNaissance;
+// dd($age);
+
+// Calcule le prix TTC 
+$prixHT = 34;
+$tva = 10;
+
+$prixTTC = $prixHT + ($tva * $prixHT) / 100;
+
+dd($prixTTC);
+
 
 require './index.view.php';
