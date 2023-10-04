273328663ca059ad52cc004138b781c3fbd7bb6a
Mehdi Key <formk@kermed.fr>
2023-10-04 10:32:32 +0200

    Initialisation

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include('./header.php');
+
+
+echo "<h1>Ceppic PHP</h1>";
+?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci dicta non nostrum totam ullam expedita, blanditiis id? Ipsa officiis delectus consequuntur ut inventore animi cupiditate, qui pariatur impedit illum.</p>
+
+<?php
+echo "Encore Ceppic PHP";
+?>
