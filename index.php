908f4dffb32d864ee0449d11c5342ab98fe54ee3
Mehdi Key <formk@kermed.fr>
2023-10-06 14:37:53 +0200

    Api json t

--- index.php
+++ index.php
@@ -23,11 +23,11 @@ dd($tabFusion[5][2]); */
 
 // Tableaux associatif
 
-$fruits = [
+/* $fruits = [
     'banane' => 'jaune',
     'pomme' => 'rouge',
     'kiwi' => 'vert'
-];
+]; */
 
 //dd($fruits['pomme']);
 /* $fruits['poire'] = 'vert';
@@ -64,10 +64,10 @@ arsort
 krsort
 */
 
-$tab = [];
+/* $tab = [];
 $tab[] = ['A', 'B', 'C'];
 $tab[] = ['Q', 'R', 'T'];
-$tab[] = ['E', 'U', 'P', 'I'];
+$tab[] = ['E', 'U', 'P', 'I']; */
 
 // dbug($tab);
 
@@ -75,7 +75,7 @@ $tab[] = ['E', 'U', 'P', 'I'];
 
 // dd($tab[0][2].$tab[2][0].$tab[2][2].$tab[2][2].$tab[2][3].$tab[0][2]);
 
-$person1 = [
+/* $person1 = [
     'nom' => 'Carle',
     'prenom' => 'Awa',
     'email' => 'cawa@wahoo.com'
@@ -93,17 +93,64 @@ $people = [
         'prenom' => 'Lucie',
         'email' => 'ludo@gimayle.com'
     ]
-];
-dbug($people);
+]; */
+// dbug($people);
 // unset($people);
 
 // Afficher les phrases suivantes :
 // Le nom de Lucie est Dupond.
 // L'email de Michel MOURAD est moumi@caramail.com.
 
-dbug('Le nom de '. $people[2]['prenom'] . ' est ' . $people[2]['nom'] . '.');
-dbug("L'email de " . $people[1]['prenom'] . ' ' . strtoupper($people[1]['nom']) . ' est ' . $people[1]['email'] . '.');
+// dbug('Le nom de '. $people[2]['prenom'] . ' est ' . $people[2]['nom'] . '.');
+// dbug("L'email de " . $people[1]['prenom'] . ' ' . strtoupper($people[1]['nom']) . ' est ' . $people[1]['email'] . '.');
 
-$jsonPeople = json_encode($people);
+// $jsonPeople = json_encode($people);
 
+// dd($jsonPeople);
+
+// $jsonPerson = '{
+//     "nom" : "Pontpasneuf",
+//     "prenom" : "Albert",
+//     "email" : "pontal@free.fr",
+//     "couleur" : ["rouge","ver","jaune"],
+//     "image" : "https://ximg.es/128x128/000/fff"
+// }';
+
+// $jsonPersonArrayPhp = json_decode($jsonPerson,true);
+
+// dbug($jsonPersonArrayPhp);
+
+// Afficher une card pour cette personne
+
+?>
+<!-- <figure>
+    <img src="<? //=$jsonPersonArrayPhp['image'] 
+                ?>">
+        <figcaption>
+            <ul>
+                <li><? //=$jsonPersonArrayPhp['prenom'] 
+                    ?> <? //=$jsonPersonArrayPhp['nom'] 
+                                                            ?></li>
+                <li><? //=$jsonPersonArrayPhp['email'] 
+                    ?></li>
+            </ul>
+        </figcaption>
+    </img>
+</figure> -->
+
+<?php
+$urlApi = 'https://pokeapi.co/api/v2/pokemon/ditto';
+$jsonApi = file_get_contents($urlApi);
+$jsonApiArray = json_decode($jsonApi,true);
+
+// dd($jsonApiArray);
+// dd($jsonApiArray['abilities'][0]['ability']['name']);
+
+// dd($jsonApiArray['sprites']['back_default']);
+?>
+<img src="<?=$jsonApiArray['sprites']['back_default']?>">
+
+
+<?php
 //require './index.view.php';
+?>
