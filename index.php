1bfc39a89b10b9d2b1af705395d64b78331caef0
Mehdi Key <formk@kermed.fr>
2023-10-12 10:26:03 +0200

    Conditions : switch et match

--- index.php
+++ index.php
@@ -20,12 +20,13 @@ $user = [
 !=
 !==
 */
-$age = 20;
+// $age = 10;
 // if($age >= 18) {
 //     echo 'Peut voter.';
 // } else {
 //     echo 'Ne peut pas voter !';
 // }
+// echo ($age >= 18) ? 'Peut voter' : 'Ne peut pas voter !';
 
 /* if ($age >= 18) :
     echo 'Peut voter.';
@@ -45,16 +46,58 @@ else:
     echo "On est l'apres-midi.";
 endif; */
 
-$a = 20;
-$b = '5';
+// $a = 20;
+// $b = '5';
 
 // if($b != 3):
 //     echo 'La variable $b n\' est pas égale à 3';
 // endif;
 
-if($b === '5'):
-    echo 'La variable $b égale à 5';
-endif;
+// if($b === '5'):
+//     echo 'La variable $b égale à 5';
+// endif;
 
+// $vrai = false;
+// $vrai = null;
+// if($vrai):
+//     echo 'Vrai';
+// else:
+//     echo 'Faux';
+// endif;
+// if ($vrai) {
+//     echo 'Vrai';
+// } else {
+//     echo 'Faux';
+// }
 
-// require './index.view.php';
+// echo $vrai ? 'Vrai' : 'Faux';
+
+// $votes = 7500;
+// dbug($votes);
+// $votesPrecedents = 1254;
+// ($votes > $votesPrecedents) ? $votes-- : $votes++;
+// dbug($votes);
+
+$couleur = 'violet';
+// switch ($couleur) {
+//     case 'rouge':
+//         echo 'Votre couleur favorite est le rouge';
+//         break;
+//     case 'bleue':
+//         echo 'Votre couleur favorite est le bleue';
+//         break;
+//     case 'vert':
+//         echo 'Votre couleur favorite est le vert';
+//         break;
+//     default:
+//         echo 'Votre couleur favorite est ni le rouge,ni le blue,ni le vert';
+// }
+
+$return_value = match($couleur) {
+    'rouge' => 'Votre couleur favorite est le rouge',
+    'bleue' => 'Votre couleur favorite est le blue',
+    'vert' => 'Votre couleur favorite est le vert',
+    default => 'Votre couleur favorite est ni le rouge,ni le blue,ni le vert'
+};
+
+echo $return_value;
