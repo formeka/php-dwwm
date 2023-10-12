4338b58f5b38bbcbfc8d8f1427b56e67546bed65
Mehdi Key <formk@kermed.fr>
2023-10-12 16:33:31 +0200

    Nettoyage variable en get et post

--- traitement.php
+++ traitement.php
@@ -4,13 +4,17 @@ require './fonctions.php';
 // dbug($_GET);
 // dbug($_POST);
 // dbug($_SERVER);
-dbug($_REQUEST);
+// dbug($_REQUEST);
+
+// dbug($_GET['nom']);
+// dbug(htmlspecialchars($_GET['nom']));
+// dbug(htmlentities($_GET['nom']));
 
 // if (isset($_POST['submitted'])) :
 if ($_SERVER['REQUEST_METHOD'] === 'POST') :
     echo "Votre nom est : <strong>" . $_POST['nom'] . "</strong>.<br>";
     echo "Votre email est : <strong>" . $_POST['email'] . "</strong>.<br>";
-    echo "Votre commentaire est : <strong>" . $_POST['commentaire'] . "</strong>.<br>";
+    echo "Votre commentaire est : <strong>" . htmlspecialchars($_POST['commentaire']) . "</strong>.<br>";
     echo "Votre niveau est : <strong>" . $_POST['niveau'] . "</strong>.<br>";
     echo "Vos competences sont : <strong>" . implode(",", $_POST['competence']) . "</strong>.<br>";
 endif;
