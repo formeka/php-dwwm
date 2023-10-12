52d138ea29a2ab1b4a61b6884abd90d4327a53e0
Mehdi Key <formk@kermed.fr>
2023-10-12 13:45:33 +0200

    Method get et post

--- fonctions.php
+++ fonctions.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 function dbug($value)
 {
-    echo '<pre style="background-color:black;color:white;overflow: auto;padding: 10px;">';
+    echo '<pre style="background-color:black;color:white;overflow: auto;padding: 1rem;font-family:monospace;">';
     print_r($value);
     // var_dump($value);
     echo '</pre>';
@@ -14,66 +14,3 @@ function dd($value)
     dbug($value);
     die('Script php arrété !!!');
 }
-
-//////////////////////////////////////////////////
-
-// function calcul($a , $b)
-// {
-//     // $c = $a + $b;
-//     // return $c;
-//     // echo 'Calcul';
-//     return $a + $b;
-// }
-
-// echo calcul(10,12);
-
-// function calcul($a , $b=12)
-// {
-//     return $a + $b;
-// }
-
-// echo calcul(10,12);
-// echo calcul(10);
-
-// Creer une fonction qui calcul le prix TTC en lui passant un prix HT et la TVA
-// function calculPrixTTC($prixHT, $tva)
-// {
-//     return $prixHT + ($prixHT * $tva) / 100;
-// }
-// echo calculPrixTTC(34,10);
-
-// Creer une fonction qui calcul l'age d'une personne en lui donnant sa date de naissance
-// function calculAge($anneNaissance)
-// {
-//     $anneeEnCours = date("Y");
-//     return $anneeEnCours - $anneNaissance;
-// }
-// echo calculAge(1990);
-
-// $calcul = function ($nb) {
-//     return $nb + 1;
-// };
-
-// echo $calcul(25);
-
-
-// function calcul(int | float $a , int | float $b): int | float
-// function calcul(int | float $a , int | float $b): mixed
-// {
-//     return $a + $b;
-// }
-
-// echo calcul(2.6,5);
-
-$x = 6;
-$y = 3;
-
-function calcul($x)
-{
-    global $y;
-    $y = 7;
-    return $x + $y;
-}
-// echo calcul(5);
-echo calcul($x);
-dd($y);
--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
-require './fonctions.php';
+<?php 
+require './fonctions.php'; 
+
+// dbug($_GET);
+dbug($_POST);
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Formulaire</title>
+</head>
+
+<body>
+    <h1>Formulaire</h1>
+    <form action="" method="POST">
+        <label for="nom">Nom :
+        <input type="text" name="nom">
+        </label>
+        <label for="email">Email :
+        <input type="email" name="email">
+        </label>
+        <input type="submit" value="Valider">
+    </form>
+</body>
+
+</html>
