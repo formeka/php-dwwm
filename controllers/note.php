e8ddba89af3509a0edfb3f0d695c4da95935c43a
Mehdi Key <formk@kermed.fr>
2023-10-25 10:26:30 +0200

    Ajout du nom du user dans la note

--- controllers/note.php
+++ controllers/note.php
@@ -1,11 +1,19 @@
 <?php
 require 'models/Database.php';
 
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) :
+    abort();
+endif;
+
 $id = $_GET['id'];
 
-$note = $connexion->prepare('SELECT * FROM note WHERE id = :id');
-$note->bindParam(':id' , $id);
+$note = $connexion->prepare('SELECT * FROM note AS n
+INNER JOIN user AS u 
+ON n.user_id = u.user_id
+WHERE n.id = :id');
+$note->bindParam(':id', $id);
 $note->execute();
 $note = $note->fetch(PDO::FETCH_ASSOC);
+
 
 require 'views/note.view.php';
--- views/note.view.php
+++ views/note.view.php
@@ -8,7 +8,7 @@
 <body>
     <h2><?=$note['title']?></h2>
     <p><?=$note['content']?></p>
-    <p><?=$note['created_at']?></p>
+    <p>Publiée le <?=$note['created_at']?> par <strong><?=$note['name']?></strong></p>
     <p><a href="/notes">Retour à la liste des notes</a></p>
 </body>
 </html>
