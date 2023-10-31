6607f0ea16f023ad9c14ddbfd1bf3403a1c2e245
Mehdi Key <formk@kermed.fr>
2023-10-31 11:39:17 +0100

    Ajout dossier admin

--- controllers/note-new.php
+++ controllers/note-new.php
@@ -8,6 +8,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') :
     $errors = [];
     //$errors = '';
 
+    dd($_POST);
+
     $title = trim(filter_var($_POST['title'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     $content = trim(filter_var($_POST['content'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     $author = trim(filter_var($_POST['author'], FILTER_SANITIZE_NUMBER_INT));
@@ -31,22 +33,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') :
     if(empty($_POST['author']) || strlen($_POST['author'] === 0)) :
         $errors[] = 'Aucun auteur séléctionné !!!';
     endif;
-
-/*     if (strlen($title) >= 100 || strlen($title) === 0) :
-        $errors[] = 'Titre trop long !!!';
-    endif; */
-
-/*     if (strlen($title) >= 100 || strlen($title) === 0) :
-        $errors[] = 'Titre trop long !!!';
-    endif;
-
-    if (strlen($content) >= 100 || strlen($content) === 0) :
-        $errors[] = 'Contenue trop long bouuuu !!!';
-    endif; */
-
-/*     if (strlen($title) >= 100 || strlen($title) === 0 || strlen($content) >= 1000 || strlen($content) === 0) :
-        $errors = 'Le titre ou le contenue de votre note est incorrect !!!';
-    endif; */
 
     if (empty($errors)) :
         $noteNew = $connexion->prepare('INSERT INTO note (title,content,user_id) VALUES (:title , :content , :user_id)');
--- views/note-new.view.php
+++ views/note-new.view.php
@@ -5,8 +5,10 @@
 <form method="POST">
     <label for="title">Titre :</label>
     <input type="text" name="title" id="title" value="<?= isset($_POST['title']) ? $_POST['title'] : '' ?>">
+
     <label for="content">Contenu :</label>
     <textarea name="content" id="content" cols="30" rows="10"><?= isset($_POST['content']) ? $_POST['content'] : '' ?></textarea>
+    
     <label for="author">Auteur :</label>
     <select name="author" id="author">
         <option value=""></option>
