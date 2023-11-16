68fa70c0e67fb5dad3dab763d4a0a2985aa645eb
Mehdi Key <formk@kermed.fr>
2023-11-16 10:37:29 +0100

    Session

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-// COOKIE
+session_start();
 
-/* $value = 'Valeur de test';
-
-//setcookie("TestCookie", $value);
-//setcookie("TestCookie", $value, time()+3600); 
-setcookie("TestCookie", $value, time()-3600); 
-
-echo $_COOKIE["TestCookie"]; */
+//$_SESSION['CouleurFavorite'] = 'vert';
+$_SESSION['isConnected'] = true;
 
 
-
-
+?>
+<p><a href="./session.php">Lire la variable session</a></p>
+<p><a href="./kill_session.php">Tuer la session</a></p>
--- session.php
+++ session.php
@@ -1,3 +1,10 @@
 <?php
 session_start();
 
+//echo $_SESSION['CouleurFavorite'];
+
+if($_SESSION['isConnected']):
+    echo 'Vous êtes bien connecté.';
+else:
+    echo 'Vous êtes déconnecté.';
+endif;
