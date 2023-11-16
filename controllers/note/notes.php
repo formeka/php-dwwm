f6fb4b1b03ffc502076ae9342875d5299130fd84
Mehdi Key <formk@kermed.fr>
2023-11-16 16:49:58 +0100

    Bug liste de notes d'un user

--- controllers/note/notes.php
+++ controllers/note/notes.php
@@ -1,6 +1,28 @@
 <?php
 require 'models/Database.php';
 
-$notes = $connexion->query('SELECT * FROM note ORDER BY id DESC')->fetchAll();
+dbug($_SESSION['isLogged']);
+dbug($_SESSION['userId']);
+
+if(isset($_SESSION['isLogged']) && isset($_SESSION['userId'])) :
+
+    $notes = $connexion->prepare('SELECT * FROM note as n 
+    INNER JOIN user as u ON n.user_id = u.user_id 
+    WHERE u.status = 1 AND u.role = `role_user` AND u.user_id = :userid 
+    ORDER BY id DESC');
+
+    $notes->bindValue(':userid', $_SESSION['userId'], PDO::PARAM_STR);
+
+    $notes->execute();
+    $notes = $notes->fetchAll(); 
+
+
+
+else:
+    header('Location: /');
+    die();
+endif;
+
+
 
 require 'views/note/notes.view.php';
